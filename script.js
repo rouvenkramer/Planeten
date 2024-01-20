1e3ba6948edf036d0768cad942d8cbf775e1e894
rouvenkramer <kramer.rouven@gmail.com>
2024-01-20 20:34:30 +0100

    script und classes angelegt

---
+++ script.js
@@ -0,0 +1,23 @@
+function divBorders() {
+    document.querySelectorAll('div').forEach(div => div.style.border = '2px solid red');
+    document.querySelectorAll('div').forEach(div => div.style.boxSizing = 'border-box');
+}
+
+function divNoBorders() {
+    document.querySelectorAll('div').forEach(div => div.style.border = 'none');
+
+}
+
+function planet2(changeTo) {
+
+    const element = document.getElementById('planet2image');
+    const allClasses = ['mercury', 'venus', 'earth', 'moon', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto'];
+
+    element.src = `./img/${changeTo}.jpg`;
+
+    element.classList.remove(...allClasses);
+
+    element.classList.add(changeTo);
+
+}
+
